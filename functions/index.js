759153ad50d2dacdb819df7072e4fd042acc0476
unknown <ri-yan@outlook.com>
2024-04-13 11:14:12 +0530

    firebase

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+// functions/index.js
+const functions = require('firebase-functions');
+const next = require('next');
+
+const nextApp = next({ dev: false, conf: { distDir: '.next' } });
+const handle = nextApp.getRequestHandler();
+
+exports.nextApp = functions.https.onRequest((req, res) => {
+  return nextApp.prepare().then(() => handle(req, res));
+});
