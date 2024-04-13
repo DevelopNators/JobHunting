66613819a04f7220a80876910049455523d52b92
mehruddin11 <mehruddin881@gmail.com>
2024-04-13 11:10:37 +0530

    changes

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -3,10 +3,10 @@ import Links from "../components/links/links";
 const PrivacyPolicy = () =>{
     return (
         <div data-theme="light">
-            <div className="term-container">
+            <div className="term-container" >
 
 
-            <div className="term-services mt-6" >
+            <div className="term-services mt-6" style= {{ position: 'relative', marginTop: 'calc(120px)'}} >
             <h1>Privacy Policy </h1>
 
             </div>
