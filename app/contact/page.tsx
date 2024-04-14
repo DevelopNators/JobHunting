553e22bbbab21e0419640a66d88da5ef0decf9fd
unknown <ri-yan@outlook.com>
2024-04-14 16:07:55 +0530

    changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { socialLinks } from '@/public/data';
 import { useEffect, useState } from 'react';
 
 export default function Contact() {
@@ -25,7 +26,7 @@ export default function Contact() {
             </div>
             <nav style={{marginTop:'-20px' ,zIndex:'200'}}>
           <div className="grid grid-flow-col gap-4">
-            <a>
+            {/* <a>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -57,7 +58,31 @@ export default function Contact() {
               >
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
               </svg>
+            </a> */}
+            <a 
+               href={socialLinks[0].url}
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="fill-current"
+              >
+                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+              </svg>
             </a>
+
+            <a href={socialLinks[2].url} target="_blank"></a>
+            <a
+              href={socialLinks[1].url}
+              target="_blank"
+            ></a>
+            <a
+               href={socialLinks[3].url}
+              target="_blank"
+            ></a>
           </div>
         </nav>
             
