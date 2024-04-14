e0a64f9695355ef8ac69d2f8164c54283210530e
unknown <ri-yan@outlook.com>
2024-04-14 16:27:54 +0530

    icon changes

--- app/header/Header.tsx
+++ app/header/Header.tsx
@@ -161,7 +161,7 @@ const Header = () => {
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a
             target="_blank"
-            href="https://developnators.com"
+            href="https://developnators.com/trainings"
             className="text-sm font-semibold leading-6 text-gray-900"
           >
             Internship <span aria-hidden="true">&rarr;</span>
