de2311e640dfc28c286287a90d3838bd1fbd6361
mehruddin11 <mehruddin881@gmail.com>
2024-04-15 11:36:04 +0530

    Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,29 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 let measurementId ='G-64SHT6GEGF'
 export const metadata: Metadata = {
-  title: 'Job Huntings',
+  title: 'Job Huntings | Your Job Hunting Platform', 
   description: "Stay ahead in your job hunt with our daily updates on the latest job openings, training opportunities, and internships. We provide timely information to help you navigate the dynamic landscape of career opportunities.",
-  icons:"../public/favicon.ico",
-  applicationName:"Job Huntings"
+  icons: "../public/favicon.ico",
+  applicationName: "Job Huntings",
+  keywords: ['job hunting', 'job openings', 'internships', 'training', 'careers'], 
+  openGraph: {
+    title: 'Job Huntings | Find Your Dream Job', 
+    description: 'Your one-stop shop for job hunting resources.',
+    url: 'https://jobhuntings.developnators.com/',
+    images: [ 
+      {
+        url: 'https://jobhuntings.developnators.com/', // Image URL
+        width: 800,
+        height: 600, 
+      },
+    ],
+    siteName: 'Job Huntings', 
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@jobhuntings', 
+  },
+ 
 };
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
@@ -27,8 +46,6 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
       <body className={inter.className}>
      
         <Header  /> 
-       
-
         <main  style={{backgroundColor:'#F7F8F9'}}>{children}</main>
         <Footer  /> 
       </body>
